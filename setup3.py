ad11ca3cb6d71463608499c9cd9ecb085ea6b3ff
rafpyprog <rafael.alves.ribeiro@gmail.com>
2017-09-20 16:25:08 -0300

    Add dependency link

--- setup3.py
+++ setup3.py
@@ -2,13 +2,13 @@ from setuptools import setup
 from mailsender.mailsender import __version__
 
 setup(
-    author="Rafael Alves Ribeiro",
-    author_email="rafael.alves.ribeiro@gmail.com",
-    name="mailsender",
-    packages=["mailsender"],
+    author='Rafael Alves Ribeiro',
+    author_email='rafael.alves.ribeiro@gmail.com',
+    name='mailsender',
+    packages=['mailsender'],
     version=__version__,
-    description="Envia email utilizando o usuário ativo no Outlook.",
-    install_requires=[
-        'pypiwin32',
+    description='Envia email utilizando o usuário ativo no Outlook.',
+    dependency_links=[
+        'https://github.com/rafpyprog/MailSender/raw/master/wheels/pywin32-221-cp36-none-win_amd64.whl'
     ]
     )
